92a066179851c74e459b43308e9740aa75484cb0
badrive <badirov.faras@gmail.com>
2025-08-23 12:47:39 +0100

    add more libraries

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cub2d
+gcc -Wall -Wextra -Werror your_file.c -lm -lmlx -lXext -lX11 -o your_program
--- pars_utils/parsing.h
+++ pars_utils/parsing.h
@@ -6,19 +6,21 @@
 /*   By: bfaras <bfaras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 12:01:33 by bfaras            #+#    #+#             */
-/*   Updated: 2025/08/23 12:02:55 by bfaras           ###   ########.fr       */
+/*   Updated: 2025/08/23 12:46:30 by bfaras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 
-# include <limits.h>
-# include <stdarg.h>
-# include <stdint.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <math.h>
+#include <mlx.h>
 
 #endif
